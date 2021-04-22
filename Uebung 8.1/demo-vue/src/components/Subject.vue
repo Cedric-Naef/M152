2fbc14d785439cd6fdee22ed68d8839a3001c267
cedric <Cedric_Naef@sluz.ch>
2021-04-22 15:13:47 +0200

    8.1

---
+++ Uebung 8.1/demo-vue/src/components/Subject.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="main">
+        <div v-for="subject in subjects" :key="subject.name">
+            <h2>{{ subject.name }}</h2>
+
+            <ul>
+                <li v-for="grade in subject.grades">{{ grade }}</li>
+            </ul>
+
+            Durchschnitt: {{ getAverage(subject.grades) }}
+            <hr />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Subject',
+  data: function() {
+            return {
+                subjects: [
+                    {
+                        name: "Mathematik",
+                        grades: [5,4.7,3.5,4.1]
+                    },
+                    {
+                        name: "Geschichte",
+                        grades: [5.5,4.9,5.75]
+                    },
+                    {
+                        name: "Physik",
+                        grades: [2,2.7,4.3]
+                    },
+                    {
+                        name: "Deutsch",
+                        grades: [4.25,3.4,3.5]
+                    },
+                ]
+            }
+        },
+        methods: {
+            getAverage: function(grades) {
+                let total = 0;
+                grades.forEach(grade => total += grade);
+                const avg = total / grades.length;
+                return Math.round(avg * 2) / 2; // Round to 0.5
+            }
+        }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.main {
+        max-width: 500px;
+        margin: 0 auto;
+        background-color: #eee;
+        padding: 20px;
+        h1 {
+            text-align: center;
+        }
+    }
+</style>
